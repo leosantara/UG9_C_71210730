86c9cb1d1cff2c1f589b8ba97a6e926c2bb4a14e
leosantara <93345048+leosantara@users.noreply.github.com>
2021-11-12 10:38:32 +0700

    Add files via upload

---
+++ 1_C_71210730.py
@@ -0,0 +1,19 @@
+#Menghitung luas lingkaran
+#jari-jari=r
+
+#jari-jari lingkaran 1 = r_1
+#jari-jari lingkaran 1 = 4
+r_1=4
+luas_1=3.14*r_1**2
+print("a. Test case 1:")
+print("Masukan jari-jari: ", r_1)
+print(luas_1)
+
+#jari-jari lingkaran 2 = r_2
+#jari-jari lingkaran 2 = 15
+r_2=15
+luas_2=3.14*r_2**2
+print("b. Test case 2:")
+print("Masukan jari-jari: ", r_2)
+print(luas_2)
+
